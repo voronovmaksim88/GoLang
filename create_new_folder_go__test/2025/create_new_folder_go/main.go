dcccc721ca7c8a2c82fcaa2e1142764860440b31
MaksimHome <voronovmaksim88@gmail.com>
2025-06-03 08:47:42 +0700

    смог подключиться к бд

---
+++ create_new_folder_go__test/2025/create_new_folder_go/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/fatih/color"
+)
+
+func main() {
+	// Загружаем .env файл
+	err := godotenv.Load()
+	if err != nil {
+		color.Red("Ошибка загрузки .env файла: %v", err)
+		os.Exit(1)
+	}
+
+	// Формируем строку подключения для MariaDB
+	connString := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("USER"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("HOST"),
+		os.Getenv("PORT"),
+		os.Getenv("NAME"),
+	)
+
+	// Пытаемся подключиться к базе данных
+	db, err := sql.Open("mysql", connString)
+	if err != nil {
+		color.Red("Ошибка открытия соединения с базой данных: %v", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	// Проверяем подключение
+	err = db.Ping()
+	if err != nil {
+		color.Red("Ошибка ping базы данных: %v", err)
+		os.Exit(1)
+	}
+
+	// Выводим сообщение об успехе
+	color.Green("Успешное подключение к базе данных MariaDB!")
+}
