da84b1253bbc78c3da5bce95236a56d0d72952a8
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-07-24 11:17:55 +0700

    Добавил пример чтения регистров по модбас на библиотеке "github.com/simonvetter/modbus"

---
+++ simonvetter_modbus/go.mod
@@ -0,0 +1,12 @@
+module simonvetter_modbus
+
+go 1.24
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/goburrow/serial v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/simonvetter/modbus v1.6.3 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ simonvetter_modbus/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/simonvetter/modbus"
+	"log"
+	"os"
+	"time"
+)
+
+func runModbus() (err error) {
+	// Отложенное восстановление после паники
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("паника: %v", r)
+		}
+	}()
+
+	cfg := &modbus.ClientConfiguration{
+		URL:      "rtu://COM7", // для windows
+		Speed:    9600,
+		DataBits: 8,
+		Parity:   modbus.PARITY_EVEN,
+		StopBits: 2,
+		Timeout:  5 * time.Second,
+	}
+
+	client, err := modbus.NewClient(cfg)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	// Открываем соединение
+	err = client.Open()
+	if err != nil {
+		return err
+	}
+
+	err = client.SetUnitId(1)
+	if err != nil {
+		return err
+	}
+
+	regs, err := client.ReadRegisters(0x0109, 6, modbus.HOLDING_REGISTER)
+	if err != nil {
+		return err
+	}
+	log.Printf("Результат: %v", regs)
+	return nil
+}
+
+func main() {
+	log.Println("Привет друг. Сейчас будем читать регистры из комбайна\n" +
+		"BUILD_Y\t0x0109\tuint16\tтолько чтение\tгод\n" +
+		"BUILD_M\t0x010A\tuint16\tтолько чтение\tмесяц\n" +
+		"BUILD_D\t0x010B\tuint16\tтолько чтение\tдень\n" +
+		"BUILD_H\t0x010C\tuint16\tтолько чтение\tчас\n" +
+		"BUILD_m\t0x010D\tuint16\tтолько чтение\tминута\n" +
+		"BUILD_S\t0x010E\tuint16\tтолько чтение\tсекунда\n" +
+		"")
+
+	// Отложенный вызов ожидания Enter
+	defer func() {
+		log.Println("Нажмите Enter для выхода...")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+	}()
+
+	// Выполнение Modbus-запроса
+	if err := runModbus(); err != nil {
+		// Вывод ошибки красным цветом
+		red := color.New(color.FgRed).SprintFunc()
+		log.Printf("%s: %v", red("Ошибка"), err)
+	}
+}
