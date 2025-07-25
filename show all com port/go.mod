214d09ea749d3fc4be395f2f383673e963a338c0
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-07-25 18:02:11 +0700

    Напромтил прогу для сканирования ком портов

---
+++ show all com port/go.mod
@@ -0,0 +1,11 @@
+module show_all_com_port
+
+go 1.24
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
---
+++ show all com port/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/tarm/serial"
+	"log"
+	"os"
+)
+
+func main() {
+	// Отложенный вызов ожидания Enter с обработкой ошибки
+	defer func() {
+		log.Println("Нажмите Enter для выхода...")
+		_, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
+		if err != nil {
+			log.Printf("Ошибка при чтении ввода: %v", err)
+		}
+	}()
+
+	// Создаем функцию для вывода текста зеленым цветом
+	green := color.New(color.FgGreen).PrintfFunc()
+
+	// Список возможных COM-портов для проверки
+	ports := []string{
+		"COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8",
+		"COM9", "COM10", "COM11", "COM12", "COM13", "COM14", "COM15",
+	}
+
+	fmt.Println("Доступные COM-порты:")
+
+	for _, port := range ports {
+		// Пытаемся открыть порт
+		config := &serial.Config{Name: port, Baud: 9600}
+		s, err := serial.OpenPort(config)
+		if err == nil {
+			// Если порт открыт успешно, выводим его имя зеленым цветом
+			green("%s\n", port)
+			// Закрываем порт и обрабатываем ошибку
+			if err := s.Close(); err != nil {
+				fmt.Printf("Ошибка при закрытии порта %s: %v\n", port, err)
+			}
+		}
+	}
+}
