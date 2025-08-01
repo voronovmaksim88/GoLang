124c943d6f8e578bcc0f11538e97dfdb39ab8fb0
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-08-01 13:52:41 +0700

    Худо бедно криво косо обновил стек на фронт и подключил primevue

--- golang+vue test select/CounterApp/frontend/src/App.vue
+++ golang+vue test select/CounterApp/frontend/src/App.vue
@@ -1,11 +1,4 @@
 <script>
-// Компоненты PrimeVue UI
-import Select from 'primevue/select';
-import Toast from 'primevue/toast';
-import Paginator from 'primevue/paginator';
-import DatePicker from 'primevue/datepicker';
-import Checkbox from 'primevue/checkbox';
-import Button from "primevue/button";
 </script>
 
 
