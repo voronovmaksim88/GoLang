6da18bf6d9d23fe4df727854a1669a267a929dd8
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-11-12 13:55:57 +0700

    переписал hello world

--- 0_hello_world/main.go
+++ 0_hello_world/main.go
@@ -4,14 +4,7 @@ import (
 	"fmt"
 )
 
-func calculateBMI(weight float64, height float64) float64 {
-	return weight / (height * height)
-}
-
 func main() {
-	var weight float64 = 92   // вес в килограммах
-	var height float64 = 1.78 // рост в метрах
 
-	bmi := calculateBMI(weight, height)
-	fmt.Printf("Ваш индекс массы тела равен %.2f\n", bmi)
+	fmt.Printf("Hello World")
 }
